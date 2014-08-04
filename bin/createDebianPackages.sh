05c092d48d971e9d0459a9b4186b4dc1dd38882c
Hélder Silva <hsilva@keep.pt>
2014-08-04 18:06:25 +0100

    Updated some toolspecs (added opf debian repository); Added scripts that ease debian generation and upload to myExperiment fo several toolspecs/components specs

---
+++ bin/createDebianPackages.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+
+if [ $# -eq 0 ]; then
+	echo "USAGE: $0 TOOLWRAPPER_BASE_DIR MAINTAINER_E-MAIL [TOOLSPECS_PATTERN]"
+	echo
+	echo "Example 1: Generate Debian packages for all toolspecs"
+	echo "   $> $0 '/path/to/toolwrapper/base/dir' 'xpto@domain.com'"
+	echo "Example 2: Generate Debian packages for migration toolspecs only"
+	echo "   $> $0 '/path/to/toolwrapper/base/dir' 'xpto@domain.com' 'digital-preservation-migration*.xml'"
+	exit 1
+fi
+
+TOOLSPECS_PATTERN="../digital-preservation-migration*.xml"
+EMAIL="xpto@domain.com"
+TOOLWRAPPER_BASE_DIR="/home/hsilva/Git/scape-toolwrapper"
+if [ $# -ge 2 ]; then
+	TOOLWRAPPER_BASE_DIR="$1"
+	EMAIL="$2"
+fi
+if [ $# -eq 3 ]; then
+	TOOLSPECS_PATTERN="$3"
+fi
+
+TIME=$(date +%Y%m%dT%H%M%S)
+DEBIANS_OUTPUT_DIR="../${TIME}_debians"
+LOGFILE="../${TIME}_debians_generation.log"
+
+if [ ! -d $DEBIANS_OUTPUT_DIR ]; then
+	mkdir $DEBIANS_OUTPUT_DIR
+fi
+if [ -f $LOGFILE ]; then
+	rm $LOGFILE
+fi
+
+for spec in $(ls ../$TOOLSPECS_PATTERN);
+do
+	operation=${spec/.xml/}
+	operation_name=${operation/..\//}
+	echo "Generating Debian package for \"$operation_name\"..."
+
+	component="${operation}.component"
+	changelog="${operation}.changelog"
+	script="${operation}.sh"
+	temp_dir=$(mktemp -d)
+
+	# create bash wrapper and workflow
+	$TOOLWRAPPER_BASE_DIR/toolwrapper-bash-generator/bin/generate.sh -t $spec -c $component -o $temp_dir &>>$LOGFILE
+	
+	if [ $? -eq 0 ]; then
+		# create debian package
+		if [ -f $script ]; then
+			cp $script $temp_dir/install/
+		fi
+		$TOOLWRAPPER_BASE_DIR/toolwrapper-bash-debian-generator/bin/generate.sh -t $spec -i $temp_dir -o $temp_dir -e $EMAIL -ch $changelog &>>$LOGFILE
+
+		if [ $? -eq 0 ]; then
+			cp $temp_dir/debian/*.deb $DEBIANS_OUTPUT_DIR
+			echo -e "\tDone!"
+		else
+			echo -e "\tError generating debian package for \"$operation_name\""
+		fi
+
+		rm -rf $temp_dir
+	else
+		echo -e "\tError generating bash wrapper and workflow for \"$operation_name\"!"
+	fi
+done
---
+++ bin/uploadComponents.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+if [ $# -eq 0 ]; then
+	echo "USAGE: $0 TOOLWRAPPER_BASE_DIR MYEXPERIMENT_USERNAME MYEXPERIMENT_PASSWORD [TOOLSPECS_PATTERN]"
+	echo
+	echo "Example 1: Uploading components for all toolspecs"
+	echo "   $> $0 '/path/to/toolwrapper/base/dir' 'username' 'password'"
+	echo "Example 2: Uploading components only for migration toolspecs"
+	echo "   $> $0 '/path/to/toolwrapper/base/dir' 'username' 'password' 'digital-preservation-migration*.xml'"
+	exit 1
+fi
+
+TOOLSPECS_PATTERN="digital-preservation-migration*.xml"
+TOOLWRAPPER_BASE_DIR="/home/hsilva/Git/scape-toolwrapper"
+if [ $# -ge 3 ]; then
+	TOOLWRAPPER_BASE_DIR="$1"
+	USERNAME="$2"
+	PASSWORD="$3"
+fi
+if [ $# -eq 4 ]; then
+	TOOLSPECS_PATTERN="$4"
+fi
+
+TIME=$(date +%Y%m%dT%H%M%S)
+LOGFILE="../${TIME}_components_upload.log"
+
+if [ -f $LOGFILE ]; then
+	rm $LOGFILE
+fi
+
+for spec in $(ls ../$TOOLSPECS_PATTERN);
+do
+	operation=${spec/.xml/}
+	operation_name=${operation/..\//}
+	echo "Uploading components for \"$operation_name\"..."
+
+	component="${operation}.component"
+	changelog="${operation}.changelog"
+	script="${operation}.sh"
+	temp_dir=$(mktemp -d)
+
+	description=$(egrep "<description>" "$spec" | head -n1 | sed 's#^\s\+<description>##;s#</description>$##')
+
+	# create bash wrapper and workflow
+	$TOOLWRAPPER_BASE_DIR/toolwrapper-bash-generator/bin/generate.sh -t $spec -c $component -o $temp_dir &>>$LOGFILE
+	
+	if [ $? -eq 0 ]; then
+
+		component_family=$(echo "$operation_name" | sed 's#^digital-preservation-[a-zA-Z]\+-##;s#-.*$##')
+		family="595"
+		case $component_family in
+			"audio")
+				family="595"
+			;;
+			"video")
+				family="595"
+			;;
+			"image")
+				family="592"
+			;;
+			"office")
+				family="601"
+			;;
+		esac
+
+		# upload component (-e 490 to share with SCAPE group, -e public for public view/download) 
+		$TOOLWRAPPER_BASE_DIR/toolwrapper-component-uploader/bin/upload.sh -c "$temp_dir/workflow/$operation_name.t2flow" -d "$description" -i "$family" -l Apache -p "$PASSWORD" -s "$temp_dir/install/$operation_name.component" -t "$temp_dir/install/$operation_name.xml" -u "$USERNAME" -e 490 &>>$LOGFILE
+
+		rm -rf $temp_dir
+	else
+		echo -e "\tError generating bash wrapper and workflow for \"$operation_name\"!" &>>$LOGFILE
+	fi
+done
